f0cffbcdabc416d22f219b9e8ba1206537195169
gmyrvold <gemguard@gmail.com>
2021-08-14 14:39:50 -0500

    computer players now move when start button is clicked

--- js/app.js
+++ js/app.js
@@ -23,25 +23,72 @@ function updateQuestion() {
 function checkAnswer() {
     let answer = document.querySelector('#answer').value
     if (answer === randomizeQuestions[0].answer) {
-        document.getElementById('horse5').style.right='1600px'
+        movePlayer()
     }
 }
+//let startPosition = 'right: 1700px;'
+// function movePlayer() {
+//     const user = document.getElementById('horse5')
+//     //.style.right.value = 'right: -100px;'
+//     let startPosition = 1700
+//     setInterval(function(){
+//         startPosition -= 100
+//         user.style.right = startPosition + 'px'
+//     },1000)
+// }
 
 function getNewQuestion() {
-    console.log(randomizeQuestions)
     randomizeQuestions.shift()
-    console.log(randomizeQuestions)
 }
 
+function moveHorse1() {
+    const comPlayers = document.getElementById('horse1')
+    let startPosition = 1700
+    setInterval(function(){
+        startPosition -= 100
+        comPlayers.style.right = startPosition + 'px'
+    },1000)
+}
+function moveHorse2() {
+    const comPlayers = document.getElementById('horse2')
+    let startPosition = 1700
+    setInterval(function(){
+        startPosition -= 100
+        comPlayers.style.right = startPosition + 'px'
+    },1000)
+}
+function moveHorse3() {
+    const comPlayers = document.getElementById('horse3')
+    let startPosition = 1700
+    setInterval(function(){
+        startPosition -= 100
+        comPlayers.style.right = startPosition + 'px'
+    },1000)
+}
+function moveHorse4() {
+    const comPlayers = document.getElementById('horse4')
+    let startPosition = 1700
+    setInterval(function(){
+        startPosition -= 100
+        comPlayers.style.right = startPosition + 'px'
+    },1000)
+}
 const randomizeQuestions = []
 for (i = 0; i < 10; i++) {
     randomizeQuestions.push(mathProblems[Math.floor(Math.random() * 3)])
 }
 updateQuestion()
-// document.querySelector('#submit').addEventListener('click',answerQuestion)
+
 const buttonSubmit = document.querySelector('#submit')
 buttonSubmit.addEventListener('click', () => {
     checkAnswer()
     getNewQuestion()
     updateQuestion()
+})
+const startButton = document.querySelector('#startGame')
+startButton.addEventListener('click', () => {
+    moveHorse1()
+    moveHorse2()
+    moveHorse3()
+    moveHorse4()
 })
